d80210efe1f5207db3375b6d58fd24cac7446d07
Andrew Welch <andrew@nystudio107.com>
2017-12-01 21:58:49 -0500

    Volumes & FastCGI Cache Bust config
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ config/fastcgi-cache-bust.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * FastCGI Cache Bust plugin for Craft CMS 3.x
+ *
+ * Bust the Nginx FastCGI Cache when entries are saved or created.
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2017 nystudio107
+ */
+
+/**
+ * FastCGI Cache Bust config.php
+ *
+ * This file exists only as a template for the FastCGI Cache Bust settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'fastcgi-cache-bust.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    // All environments
+    '*' => [
+        // Enter the full absolute path to the FastCGI Cache directory.
+        "cachePath" => '',
+    ],
+
+    // Live (production) environment
+    'live'  => [
+        // Enter the full absolute path to the FastCGI Cache directory.
+        "cachePath" => '/var/run/nginx-cache/devmode',
+    ],
+
+    // Staging (pre-production) environment
+    'staging'  => [
+        // Enter the full absolute path to the FastCGI Cache directory.
+        "cachePath" => '',
+    ],
+
+    // Local (development) environment
+    'local'  => [
+        // Enter the full absolute path to the FastCGI Cache directory.
+        "cachePath" => '',
+    ],
+];
