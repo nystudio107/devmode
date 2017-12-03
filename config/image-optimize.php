29f2897b1ce188a155e29282937a8616ccf1233a
Andrew Welch <andrew@nystudio107.com>
2017-12-03 02:02:27 -0500

    ImageOptimize config
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ config/image-optimize.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * ImageOptimize plugin for Craft CMS 3.x
+ *
+ * Automatically optimize images after they've been transformed
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2017 nystudio107
+ */
+
+/**
+ * ImageOptimize config.php
+ *
+ * This file exists only as a template for the ImageOptimize settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as
+ * 'image-optimize.php' and make your changes there to override default
+ * settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just
+ * as
+ * you do for 'general.php'
+ */
+
+return [
+    //  What transform method should be used for image transforms?
+    'transformMethod' => 'craft',
+
+    // Domain for the Imgix transform service
+    'imgixDomain' => '',
+
+    // API key for the Imgix transform service
+    'imgixApiKey' => '',
+
+    // Should image variant be created on Asset save (aka BeforePageLoad)
+    'generateTransformsBeforePageLoad' => true,
+
+    // Controls whether a dominant color palette should be created for image variants
+    // It takes a bit of time, so if you never plan to use it, you can turn it off
+    'createColorPalette' => true,
+
+     // Controls whether SVG placeholder silhouettes should be created for image variants
+     // It takes a bit of time, so if you never plan to use them, you can turn it off
+    'createPlaceholderSilhouettes' => true,
+
+    // Default image variants
+    'defaultVariants'            => [
+        [
+            'width'          => 1170,
+            'useAspectRatio' => true,
+            'aspectRatioX'   => 16.0,
+            'aspectRatioY'   => 9.0,
+            'retinaSizes'    => ['1'],
+            'quality'        => 82,
+            'format'         => 'jpg',
+        ],
+        [
+            'width'          => 970,
+            'useAspectRatio' => true,
+            'aspectRatioX'   => 16.0,
+            'aspectRatioY'   => 9.0,
+            'retinaSizes'    => ['1'],
+            'quality'        => 82,
+            'format'         => 'jpg',
+        ],
+        [
+            'width'          => 750,
+            'useAspectRatio' => true,
+            'aspectRatioX'   => 4.0,
+            'aspectRatioY'   => 3.0,
+            'retinaSizes'    => ['1'],
+            'quality'        => 60,
+            'format'         => 'jpg',
+        ],
+        [
+            'width'          => 320,
+            'useAspectRatio' => true,
+            'aspectRatioX'   => 4.0,
+            'aspectRatioY'   => 3.0,
+            'retinaSizes'    => ['1'],
+            'quality'        => 60,
+            'format'         => 'jpg',
+        ],
+    ],
+
+    // Active image processors
+    'activeImageProcessors'      => [
+        'jpg' => [
+            'mozjpeg',
+        ],
+        'png' => [
+            'optipng',
+        ],
+        'svg' => [
+            'svgo',
+        ],
+        'gif' => [
+            'gifsicle',
+        ],
+    ],
+
+    // Active image variant creators
+    'activeImageVariantCreators' => [
+        'jpg' => [
+            'cwebp',
+        ],
+        'png' => [
+            'cwebp',
+        ],
+        'gif' => [
+            'cwebp',
+        ],
+    ],
+
+    // Preset image processors
+    'imageProcessors'            => [
+        // jpeg optimizers
+        'jpegoptim' => [
+            'commandPath'           => '/usr/bin/jpegoptim',
+            'commandOptions'        => '-s',
+            'commandOutputFileFlag' => '',
+        ],
+        'mozjpeg'   => [
+            'commandPath'           => '/usr/bin/mozjpeg',
+            'commandOptions'        => '-optimize -copy none',
+            'commandOutputFileFlag' => '-outfile',
+        ],
+        'jpegtran'  => [
+            'commandPath'           => '/usr/bin/jpegtran',
+            'commandOptions'        => '-optimize -copy none',
+            'commandOutputFileFlag' => '',
+        ],
+        // png optimizers
+        'optipng'   => [
+            'commandPath'           => '/usr/bin/optipng',
+            'commandOptions'        => '-o7 -strip all',
+            'commandOutputFileFlag' => '',
+        ],
+        'pngcrush'  => [
+            'commandPath'           => '/usr/bin/pngcrush',
+            'commandOptions'        => '-brute -ow',
+            'commandOutputFileFlag' => '',
+        ],
+        'pngquant'  => [
+            'commandPath'           => '/usr/bin/pngquant',
+            'commandOptions'        => '--strip--skip -if-larger',
+            'commandOutputFileFlag' => '',
+        ],
+        // svg optimizers
+        'svgo'      => [
+            'commandPath'           => '/usr/bin/svgo',
+            'commandOptions'        => '',
+            'commandOutputFileFlag' => '',
+        ],
+        // gif optimizers
+        'gifsicle'  => [
+            'commandPath'           => '/usr/bin/gifsicle',
+            'commandOptions'        => '-O3 -k 256',
+            'commandOutputFileFlag' => '',
+        ],
+    ],
+
+    'imageVariantCreators' => [
+        // webp variant creator
+        'cwebp' => [
+            'commandPath'           => '/usr/bin/cwebp',
+            'commandOptions'        => '',
+            'commandOutputFileFlag' => '-o',
+            'commandQualityFlag'    => '-q',
+            'imageVariantExtension' => 'webp',
+        ],
+    ],
+
+];
