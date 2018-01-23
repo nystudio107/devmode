01ac85bec66710961472cf3fa6b583866266f8a0
Andrew Welch <andrew@nystudio107.com>
2018-01-23 14:53:39 -0500

    Updated site-module
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ modules/sitemodule/src/SiteModule.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Site module for Craft CMS 3.x
+ *
+ * An example module for Craft CMS 3 that lets you enhance your websites with a custom site module
+ *
+ * @link      https://nystudio107.com/
+ * @copyright Copyright (c) 2018 nystudio107
+ */
+
+namespace modules\sitemodule;
+
+use modules\sitemodule\assetbundles\sitemodule\SiteModuleAsset;
+
+use Craft;
+use craft\events\RegisterTemplateRootsEvent;
+use craft\events\TemplateEvent;
+use craft\i18n\PhpMessageSource;
+use craft\web\View;
+
+use yii\base\Event;
+use yii\base\InvalidConfigException;
+use yii\base\Module;
+
+/**
+ * Class SiteModule
+ *
+ * @author    nystudio107
+ * @package   SiteModule
+ * @since     1.0.0
+ *
+ */
+class SiteModule extends Module
+{
+    // Static Properties
+    // =========================================================================
+
+    /**
+     * @var SiteModule
+     */
+    public static $instance;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct($id, $parent = null, array $config = [])
+    {
+        Craft::setAlias('@sitemodule', $this->getBasePath());
+        $this->controllerNamespace = 'sitemodule\controllers';
+
+        // Translation category
+        $i18n = Craft::$app->getI18n();
+        /** @noinspection UnSafeIsSetOverArrayInspection */
+        if (!isset($i18n->translations[$id]) && !isset($i18n->translations[$id.'*'])) {
+            $i18n->translations[$id] = [
+                'class' => PhpMessageSource::class,
+                'sourceLanguage' => 'en-US',
+                'basePath' => '@sitemodule/translations',
+                'forceTranslation' => true,
+                'allowOverrides' => true,
+            ];
+        }
+
+        // Base template directory
+        Event::on(View::class, View::EVENT_REGISTER_CP_TEMPLATE_ROOTS, function (RegisterTemplateRootsEvent $e) {
+            if (is_dir($baseDir = $this->getBasePath().DIRECTORY_SEPARATOR.'templates')) {
+                $e->roots[$this->id] = $baseDir;
+            }
+        });
+
+        // Set this as the global instance of this module class
+        static::setInstance($this);
+
+        parent::__construct($id, $parent, $config);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        self::$instance = $this;
+
+        if (Craft::$app->getRequest()->getIsCpRequest()) {
+            Event::on(
+                View::class,
+                View::EVENT_BEFORE_RENDER_TEMPLATE,
+                function (TemplateEvent $event) {
+                    try {
+                        Craft::$app->getView()->registerAssetBundle(SiteModuleAsset::class);
+                    } catch (InvalidConfigException $e) {
+                        Craft::error(
+                            'Error registering AssetBundle - '.$e->getMessage(),
+                            __METHOD__
+                        );
+                    }
+                }
+            );
+        }
+
+        Craft::info(
+            Craft::t(
+                'site-module',
+                '{name} module loaded',
+                ['name' => 'Site']
+            ),
+            __METHOD__
+        );
+    }
+
+    // Protected Methods
+    // =========================================================================
+}
