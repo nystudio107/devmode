5e6975f6e251e8ce4ffe058e42678f873aecd9e2
Andrew Welch <andrew@nystudio107.com>
2019-03-18 09:58:22 -0400

    Updated to add FauxCraftVariable
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/php/FauxTwigExtension.php
+++ src/php/FauxTwigExtension.php
@@ -8,6 +8,9 @@
  * It will provide full auto-complete for craft.app. and and many other useful things
  * in your Twig templates.
  *
+ * Documented in the article:
+ * https://nystudio107.com/blog/auto-complete-craft-cms-3-apis-in-twig-with-phpstorm
+ *
  * Place the file somewhere in your project or include it via PhpStorm Settings -> Include Path.
  * You never call it, it's never included anywhere via PHP directly nor does it affect other
  * classes or Twig in any way. But PhpStorm will index it, and think all those variables
@@ -18,13 +21,40 @@
  *
  * @link      https://nystudio107.com
  * @copyright Copyright (c) 2019 nystudio107
+ * @license   MIT
+ * @license   https://opensource.org/licenses/MIT MIT Licensed
  */
 
 namespace nystudio107\craft;
 
 /**
+ * Class FauxCraftVariable extends the actual Craft Variable, but with added properties
+ * that reflect things that are added to the Craft Variable dynamically by
+ * plugins or modules.
+ *
  * @author    nystudio107
- * @package   FauxTwigExtension
+ * @package   nystudio107\craft
+ * @since     1.0.0
+ */
+class FauxCraftVariable extends \craft\web\twig\variables\CraftVariable
+{
+    /**
+     * @var \putyourlightson\blitz\variables\BlitzVariable
+     */
+    public $blitz;
+
+    /**
+     * @var \nystudio107\twigpack\variables\ManifestVariable
+     */
+    public $twigpack;
+}
+
+/**
+ * Class FauxTwigExtension provides a faux Twig extension for PhpStorm to index
+ * so that we get Intellisense auto-complete in our Twig templates.
+ *
+ * @author    nystudio107
+ * @package   nystudio107\craft
  * @since     1.0.0
  */
 class FauxTwigExtension extends \Twig\Extension\AbstractExtension implements \Twig\Extension\GlobalsInterface
@@ -33,7 +63,7 @@ class FauxTwigExtension extends \Twig\Extension\AbstractExtension implements \Tw
     {
         return [
             // Craft Variable
-            'craft' => new \craft\web\twig\variables\CraftVariable(),
+            'craft' => new FauxCraftVariable(),
             // Craft Elements
             'asset' => new \craft\elements\Asset(),
             'category' => new \craft\elements\Category(),
