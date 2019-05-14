73d384ee1a39c24448c9c7c88d9b16cb4160f237
Andrew Welch <andrew@nystudio107.com>
2019-05-14 16:31:02 -0400

    Rebuilt assets
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ web/dist/js/episodecell.fc22c56f834f1db27b08.js
@@ -0,0 +1,11 @@
+/*!
+ * @project        Example Project
+ * @name           episodecell.fc22c56f834f1db27b08.js
+ * @author         Andrew Welch
+ * @build          Tue, May 14, 2019 8:18 PM ET
+ * @release        f390baceb5212b7e449dfeaedaab8a2aa5c6e3de [develop]
+ * @copyright      Copyright (c) 2019 Example Company, Inc.
+ *
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{100:function(t,e,s){"use strict";s.r(e);var l=function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"my-8 p-8 bg-grey-lightest"},[s("div",{staticClass:"flex flex-wrap items-center justify-center flex-row-reverse md:flex-row flex-col-reverse"},[s("div",{staticClass:" w-full md:w-2/3 text-left py-2"},[s("h2",{staticClass:"font-mono italic font-bold text-devmode-pink text-3xl"},[s("a",{staticClass:"title-link",attrs:{href:t.url}},[t._v("\n                    "+t._s(t.title)+"\n                ")])])]),t._v(" "),s("div",{staticClass:"w-full md:w-1/3 text-devmode-pink font-mono text-xs text-left md:text-right uppercase py-2"},[t._v("\n            Episode "+t._s(t.episodeNumber)+" / "+t._s(t.postDate)+"\n        ")])]),t._v(" "),s("p",{staticClass:"text-devmode-pink font-sans text-xl py-2 leading-normal"},[t._v("\n        "+t._s(t.summary)+"\n    ")])])};l._withStripped=!0;var i={name:"episode-cell",props:{title:String,url:String,postDate:String,episodeNumber:Number,summary:String}},n=s(26),r=Object(n.a)(i,l,[],!1,null,null,null);r.options.__file="src/vue/EpisodeCell.vue";e.default=r.exports}}]);
+//# sourceMappingURL=episodecell.fc22c56f834f1db27b08.js.map
