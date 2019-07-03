13ca9eaf5ce053625b3b7b665e3b17cfbbec1c9d
Andrew Welch <andrew@nystudio107.com>
2019-07-03 11:03:11 -0400

    Fix webmentions
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- modules/sitemodule/src/variables/SiteVariable.php
+++ modules/sitemodule/src/variables/SiteVariable.php
@@ -10,6 +10,11 @@
 
 namespace modules\sitemodule\variables;
 
+use Craft;
+use craft\helpers\Json;
+
+use yii\base\Exception;
+
 use modules\sitemodule\SiteModule;
 
 /**
@@ -21,6 +26,38 @@ class SiteVariable
 {
     // Public Methods
     // =========================================================================
+
+    /**
+     * @param string $url
+     *
+     * @return mixed|null
+     */
+    public function webmentions(string $url)
+    {
+        $client = Craft::$app->api->client;
+        $result = null;
+        try {
+            $response = $client->get($url);
+            $statusCode = $response->getStatusCode();
+            if ($statusCode === 200) {
+                $result = $response->getBody()->getContents();
+                if ($result) {
+                    $result = Json::decodeIfJson($result);
+                }
+                // Return null if the JSON decode fails
+                if (\is_string($result)) {
+                    $result = null;
+                }
+            } else {
+                $reason = $response->getReasonPhrase();
+                throw new Exception("Invalid response from API: ${reason} -- ${url}", $statusCode);
+            }
+        } catch (\Exception $e) {
+            Craft::error($e->getMessage() . " -- ${url}", 'site-module');
+        }
+
+        return $result;
+    }
 
     /**
      * Return the URL to the calendar feed
