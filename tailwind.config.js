9b1569acb764a4d807cce938ad1398114a53cf09
Andrew Welch <andrew@nystudio107.com>
2019-08-09 22:51:41 -0400

    Fix formatting
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  theme: {
-    // Extend the default Tailwind config
-    extend: {
-      colors: {
-        'devmode-pink': '#eb5286',
-        'devmode-aqua': '#00b2c2',
-        'devmode-yellow': '#f2d024',
-      },
-      fontSize: {
-        xxs: '0.5rem',
-        huge: '5rem',
-      },
+    theme: {
+        // Extend the default Tailwind config
+        extend: {
+            colors: {
+                'devmode-pink': '#eb5286',
+                'devmode-aqua': '#00b2c2',
+                'devmode-yellow': '#f2d024',
+            },
+            fontSize: {
+                xxs: '0.5rem',
+                huge: '5rem',
+            },
+        },
+        // Replace the default Tailwind config
+        screens: {
+            sm: '576px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px',
+        },
     },
-    // Replace the default Tailwind config
-    screens: {
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
-    },
-  },
-  corePlugins: {},
-  plugins: [],
+    corePlugins: {},
+    plugins: [],
 };
