39dbe1526d9480e1290f4c5d711a8f70bc1bf9ca
Andrew Welch <andrew@nystudio107.com>
2019-08-12 12:43:50 -0400

    Added helpers/Config
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ modules/sitemodule/src/config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Site module for Craft CMS 3.x
+ *
+ * Custom site module for the devMode.fm website
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2018 nystudio107
+ */
+
+/**
+ * Site module config.php
+ *
+ * This file exists to store config settings for the site module. This file can
+ * be used in place, or it can be put into @craft/config/
+ *
+ * This file is multi-environment aware as
+ * well, so you can have different settings groups for each environment, just
+ * as
+ * you do for 'general.php'
+ */
+return [
+];
