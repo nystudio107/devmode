f5dc1f18681fc577c6ea17b3e7c5ff7d015be6fb
Andrew Welch <andrew@nystudio107.com>
2019-08-17 00:11:00 -0400

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ web/dist/js/vueclickaway.b52f040c7067cc027ca7.js
@@ -0,0 +1,11 @@
+/*!
+ * @project        Example Project
+ * @name           vueclickaway.b52f040c7067cc027ca7.js
+ * @author         Andrew Welch
+ * @build          Sat, Aug 17, 2019 4:08 AM ET
+ * @release        15931128788c3a5d5c19a1461a45804372a931f2 [develop]
+ * @copyright      Copyright (c) 2019 Example Company, Inc.
+ *
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{91:function(e,n,t){"use strict";var i=t(74);i="default"in i?i.default:i;/^2\./.test(i.version)||i.util.warn("VueClickaway 2.2.2 only supports Vue 2.x, and does not support Vue "+i.version);var o="_vue_clickaway_handler";function a(e,n,t){c(e);var i=t.context,a=n.value;if("function"==typeof a){var u=!1;setTimeout(function(){u=!0},0),e[o]=function(n){var t=n.path||(n.composedPath?n.composedPath():void 0);if(u&&(t?t.indexOf(e)<0:!e.contains(n.target)))return a.call(i,n)},document.documentElement.addEventListener("click",e[o],!1)}}function c(e){document.documentElement.removeEventListener("click",e[o],!1),delete e[o]}var u={bind:a,update:function(e,n){n.value!==n.oldValue&&a(e,n)},unbind:c},d={directives:{onClickaway:u}};n.version="2.2.2",n.directive=u,n.mixin=d}}]);
+//# sourceMappingURL=vueclickaway.b52f040c7067cc027ca7.js.map
