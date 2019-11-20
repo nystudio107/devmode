0c4159a00d85385af84a949f4bd27a82041898ad
Andrew Welch <andrew@nystudio107.com>
2019-11-20 11:24:05 -0500

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ web/dist/js/episodecell.87180077457d66a1ec6d.js
@@ -0,0 +1,11 @@
+/*!
+ * @project        Example Project
+ * @name           episodecell.87180077457d66a1ec6d.js
+ * @author         Andrew Welch
+ * @build          Wed, Nov 20, 2019 3:27 PM ET
+ * @release        b683044ff8b47be911760828c796ca71b913ef71 [develop]
+ * @copyright      Copyright (c) 2019 Example Company, Inc.
+ *
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{150:function(e,t,s){"use strict";s.r(t);var l=function(){var e=this,t=e.$createElement,s=e._self._c||t;return s("div",{staticClass:"my-8 p-8 devmode-container"},[s("div",{staticClass:"flex flex-wrap items-center justify-center flex-row-reverse md:flex-row flex-col-reverse"},[s("div",{staticClass:" w-full md:w-2/3 text-left py-2"},[s("h2",{staticClass:"font-mono italic font-bold text-devmode-pink text-3xl"},[s("a",{staticClass:"title-link",attrs:{href:e.url}},[e._v("\n                    "+e._s(e.title)+"\n                ")])])]),e._v(" "),s("div",{staticClass:"w-full md:w-1/3 text-devmode-pink font-mono text-xs text-left md:text-right uppercase py-2"},[e._v("\n            Episode "+e._s(e.episodeNumber)+" / "+e._s(e.postDate)+"\n        ")])]),e._v(" "),s("p",{staticClass:"text-devmode-pink font-sans text-xl py-2 leading-normal"},[e._v("\n        "+e._s(e.summary)+"\n    ")])])};l._withStripped=!0;var i={name:"episode-cell",props:{title:String,url:String,postDate:String,episodeNumber:Number,summary:String}},n=s(38),r=Object(n.a)(i,l,[],!1,null,null,null);r.options.__file="src/vue/EpisodeCell.vue";t.default=r.exports}}]);
+//# sourceMappingURL=episodecell.87180077457d66a1ec6d.js.map
--- web/sw.js
+++ web/sw.js
@@ -15,7 +15,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 
 importScripts(
   "/dist/js/workbox-catch-handler.js",
-  "/dist/js/precache-manifest.dc20bb8f63d95ae8deccf0148371016e.js"
+  "/dist/js/precache-manifest.d3829dd61c409ca3cb8879f11ea06f39.js"
 );
 
 /**
