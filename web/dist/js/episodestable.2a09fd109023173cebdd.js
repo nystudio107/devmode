9fd2850b8f997b81ba48349e3b9790f3aea2235a
Andrew Welch <andrew@nystudio107.com>
2019-12-23 05:00:47 -0500

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- web/dist/js/episodestable.2a09fd109023173cebdd.js
+++ web/dist/js/episodestable.2a09fd109023173cebdd.js
@@ -2,8 +2,8 @@
  * @project        Example Project
  * @name           episodestable.2a09fd109023173cebdd.js
  * @author         Andrew Welch
- * @build          Mon, Dec 23, 2019 1:40 AM ET
- * @release        7b8a93dbeb65daf2733d1b17d70877b28ac0ff04 [develop]
+ * @build          Mon, Dec 23, 2019 9:58 AM ET
+ * @release        9cab16bbb6809811b8b7077f20ad657734899eb0 [develop]
  * @copyright      Copyright (c) 2019 Example Company, Inc.
  *
  */
