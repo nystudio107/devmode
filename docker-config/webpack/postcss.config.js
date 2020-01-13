8262cb95cc65d1d0af95df28fe1890b4522f95ee
Andrew Welch <andrew@nystudio107.com>
2020-01-12 20:50:09 -0500

    Remove stylelint

--- docker-config/webpack/postcss.config.js
+++ docker-config/webpack/postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
     plugins: [
         require('postcss-import')({
             plugins: [
-                require('stylelint')
             ]
         }),
         require('tailwindcss')('./tailwind.config.js'),
