67a47dac7e17a85daabd1a2fb6f82f79463ef67d
Andrew Welch <andrew@keluli.local>
2020-04-27 11:59:14 -0400

    Remove lazy loading from player

--- src/js/modules/player.js
+++ src/js/modules/player.js
@@ -1,6 +1,3 @@
-import lazyLoadComponent from '../utils/lazy-load-component';
-import SkeletonBox from '../../vue/SkeletonBox.vue';
-
 // App main
 const main = async() => {
     // Async load the vue module
@@ -9,11 +6,7 @@ const main = async() => {
     const vm = new Vue({
         el: "#amplitude-player",
         components: {
-            'amplitude-player': lazyLoadComponent({
-                componentFactory: () => import(/* webpackChunkName: "amplitudeplayer" */ '../../vue/amplitude-player.vue'),
-                loading: SkeletonBox,
-                loadingData: { height: `109px`, width: `100%` }
-            }),
+            'amplitude-player': () => import(/* webpackChunkName: "amplitudeplayer" */ '../../vue/amplitude-player.vue'),
         },
         data: {
         },
