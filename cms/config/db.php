42126b11f163232578261334f5c5e501787a29fe
Andrew Welch <andrew@keluli.local>
2020-07-20 10:47:59 -0400

    Sync up db.php settings

--- cms/config/db.php
+++ cms/config/db.php
@@ -11,7 +11,11 @@
 use craft\helpers\App;
 
 return [
-    'dsn' => App::env('DB_DSN'),
+    'dsn' => App::env('DB_DSN') ?: null,
+    'driver' => App::env('DB_DRIVER'),
+    'server' => App::env('DB_SERVER'),
+    'port' => App::env('DB_PORT'),
+    'database' => App::env('DB_DATABASE'),
     'user' => App::env('DB_USER'),
     'password' => App::env('DB_PASSWORD'),
     'schema' => App::env('DB_SCHEMA'),
