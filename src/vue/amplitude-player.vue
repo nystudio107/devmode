a3735038a47c6f77feddaf00711c42a68e944292
Andrew Welch <andrew@keluli.local>
2020-08-14 10:37:42 -0400

    ESLint formatting

--- src/vue/amplitude-player.vue
+++ src/vue/amplitude-player.vue
@@ -1,79 +1,87 @@
 <template>
-    <div id="single-song-player" aria-label="Audio Player" role="region">
-        <div class="bottom-container flex items-center">
-            <div class="control-container flex-shrink">
-                <button
-                        class="amplitude-play-pause"
-                        v-on:click.prevent="playing = !playing"
-                        :title="playing == true ? 'Pause' : 'Play'"
-                        :aria-label="playing == true ? 'Pause' : 'Play'"
-                        href="#"
-                        id="play-pause"
-                        aria-controls="audiofile"
-                        role="button"
-                >
-                    <div class="text-devmode-pink text-4xl cursor-pointer">
-                        <i class="icon-play" v-show="!playing"></i>
-                        <i class="icon-pause" v-show="playing"></i>
-                    </div>
-                </button>
-                <div class="block sm:hidden text-center time-container pr-3 pl-1">
-                    <span class="current-time text-devmode-pink font-mono text-xs text-left">
-                        &nbsp;
-                    </span>
-                </div>
-            </div>
-
-            <div class="hidden sm:block time-container flex-shrink pr-3 pl-1">
-                <span class="current-time text-devmode-pink font-mono text-xs text-left">
-                    {{ currentTime }}
-                </span>
-            </div>
-
-            <div class="flex-grow px-1 pt-0">
-                <input
-                        type="range"
-                        min="0"
-                        :max="durationSeconds"
-                        v-model="currentSeconds"
-                        @input="seek"
-                        class="amplitude-song-played-progress w-full h-3 rounded"
-                        id="song-played-progress"
-                        :style="rangeStyle"
-                        tabIndex="0"
-                        title="Audio seek bar"
-                        aria-valuetext="Audio seek bar"
-                        :aria-valuemax="durationSeconds"
-                        aria-valuemin="0"
-                        :aria-valuenow="Math.round(currentSeconds)"
-                        aria-controls="audiofile"
-                        role="slider"
-                />
-                <div class="block sm:hidden text-center time-container pr-3 pl-1">
-                    <span class="current-time text-devmode-pink font-mono text-xs text-left">
-                        {{ currentTime }} / {{ durationTime }}
-                    </span>
-                </div>
-            </div>
-
-            <div class="hidden sm:block time-container flex-shrink pl-3 pr-1">
-                <span class="duration text-devmode-pink font-mono text-xs text-left">
-                    {{ durationTime }}
-                </span>
-            </div>
-
-        </div>
-        <audio
-                id="audiofile"
-                :loop="innerLoop"
-                ref="audiofile"
-                :src="url"
-                preload="auto"
-                style="display: none;"
-                :autoplay="autoPlay"
+  <div
+    id="single-song-player"
+    aria-label="Audio Player"
+    role="region"
+  >
+    <div class="bottom-container flex items-center">
+      <div class="control-container flex-shrink">
+        <button
+          id="play-pause"
+          class="amplitude-play-pause"
+          :title="playing == true ? 'Pause' : 'Play'"
+          :aria-label="playing == true ? 'Pause' : 'Play'"
+          href="#"
+          aria-controls="audiofile"
+          role="button"
+          @click.prevent="playing = !playing"
         >
-        </audio>
+          <div class="text-devmode-pink text-4xl cursor-pointer">
+            <i
+              v-show="!playing"
+              class="icon-play"
+            />
+            <i
+              v-show="playing"
+              class="icon-pause"
+            />
+          </div>
+        </button>
+        <div class="block sm:hidden text-center time-container pr-3 pl-1">
+          <span class="current-time text-devmode-pink font-mono text-xs text-left">
+                        &nbsp;
+          </span>
+        </div>
+      </div>
+
+      <div class="hidden sm:block time-container flex-shrink pr-3 pl-1">
+        <span class="current-time text-devmode-pink font-mono text-xs text-left">
+          {{ currentTime }}
+        </span>
+      </div>
+
+      <div class="flex-grow px-1 pt-0">
+        <input
+          id="song-played-progress"
+          v-model="currentSeconds"
+          type="range"
+          min="0"
+          :max="durationSeconds"
+          class="amplitude-song-played-progress w-full h-3 rounded"
+          :style="rangeStyle"
+          tabIndex="0"
+          title="Audio seek bar"
+          aria-valuetext="Audio seek bar"
+          :aria-valuemax="durationSeconds"
+          aria-valuemin="0"
+          :aria-valuenow="Math.round(currentSeconds)"
+          aria-controls="audiofile"
+          role="slider"
+          @input="seek"
+        >
+        <div class="block sm:hidden text-center time-container pr-3 pl-1">
+          <span class="current-time text-devmode-pink font-mono text-xs text-left">
+            {{ currentTime }} / {{ durationTime }}
+          </span>
+        </div>
+      </div>
+
+      <div class="hidden sm:block time-container flex-shrink pl-3 pr-1">
+        <span class="duration text-devmode-pink font-mono text-xs text-left">
+          {{ durationTime }}
+        </span>
+      </div>
     </div>
+    <audio
+      id="audiofile"
+      ref="audiofile"
+      :loop="innerLoop"
+      :src="url"
+      preload="auto"
+      style="display: none;"
+      :autoplay="autoPlay"
+    />
+  </div>
 </template>
 
 <script>
@@ -132,6 +140,22 @@
                 this.audio.volume = this.volume / 100;
             }
         },
+        created() {
+            this.innerLoop = this.loop;
+        },
+        mounted() {
+            this.audio = this.$el.querySelectorAll('audio')[0];
+            this.audio.addEventListener('timeupdate', this.update);
+            this.audio.addEventListener('loadeddata', this.load);
+            this.audio.addEventListener('pause', () => { this.playing = false; });
+            this.audio.addEventListener('play', () => { this.playing = true; });
+            if (window.location.hash.length) {
+                this.audio.addEventListener('canplaythrough', (e) => {
+                    this.currentSeconds = parseInt(window.location.hash.slice(1));
+                    this.seek();
+                }, {once: true});
+            }
+        },
         methods: {
             download() {
                 this.stop();
@@ -164,22 +188,6 @@
                 this.currentSeconds = parseInt(this.audio.currentTime);
                 window.location.hash = '#' + this.currentSeconds.toString();
             },
-        },
-        created() {
-            this.innerLoop = this.loop;
-        },
-        mounted() {
-            this.audio = this.$el.querySelectorAll('audio')[0];
-            this.audio.addEventListener('timeupdate', this.update);
-            this.audio.addEventListener('loadeddata', this.load);
-            this.audio.addEventListener('pause', () => { this.playing = false; });
-            this.audio.addEventListener('play', () => { this.playing = true; });
-            if (window.location.hash.length) {
-                this.audio.addEventListener('canplaythrough', (e) => {
-                    this.currentSeconds = parseInt(window.location.hash.slice(1));
-                    this.seek();
-                }, {once: true});
-            }
         }
     }
 </script>
