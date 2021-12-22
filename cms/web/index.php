6225ddc44affc354e281f0c421c6812041d7bab2
Andrew Welch <andrew@keluli.local>
2021-12-22 13:06:01 -0500

    feat: Use unified bootstrap.php

--- cms/web/index.php
+++ cms/web/index.php
@@ -3,22 +3,10 @@
  * Craft web bootstrap file
  */
 
-// Set path constants
-define('CRAFT_BASE_PATH', dirname(__DIR__));
-define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
-
-// Load Composer's autoloader
-require_once CRAFT_VENDOR_PATH.'/autoload.php';
-
-// Load dotenv?
-if (file_exists(CRAFT_BASE_PATH.'/.env')) {
-    (new Dotenv\Dotenv(CRAFT_BASE_PATH))->load();
-}
-
-// Set license key via .env
-define('CRAFT_LICENSE_KEY', getenv('LICENSE_KEY'));
+// Load shared bootstrap
+require dirname(__DIR__) . '/bootstrap.php';
 
 // Load and run Craft
-define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
-$app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
+/** @var craft\web\Application $app */
+$app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 $app->run();
