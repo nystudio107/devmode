d9410d10d0fc0d2caa37b5ea72e62a6a78bbc47f
Andrew Welch <andrew@keluli.local>
2022-01-20 14:50:41 -0500

    refactor: use .ts and defineConfig()

--- buildchain/vite.config.ts
+++ buildchain/vite.config.ts
@@ -1,16 +1,18 @@
-import {createVuePlugin} from 'vite-plugin-vue2'
-import legacy from '@vitejs/plugin-legacy'
+import {defineConfig} from 'vite';
+import {createVuePlugin} from 'vite-plugin-vue2';
+import legacy from '@vitejs/plugin-legacy';
 import ViteRestart from 'vite-plugin-restart';
 import viteCompression from 'vite-plugin-compression';
+import manifestSRI from 'vite-plugin-manifest-sri';
 import {visualizer} from 'rollup-plugin-visualizer';
 import eslintPlugin from 'vite-plugin-eslint';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import critical from 'rollup-plugin-critical';
 import {ViteFaviconsPlugin} from "vite-plugin-favicon2";
-import path from 'path';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
-export default ({command}) => ({
+export default defineConfig(({command}) => ({
   base: command === 'serve' ? '' : '/dist/',
   build: {
     emptyOutDir: true,
@@ -67,6 +69,7 @@ export default ({command}) => ({
     viteCompression({
       filter: /\.(js|mjs|json|css|map)$/i
     }),
+    manifestSRI(),
     visualizer({
       filename: '../cms/web/dist/stats.html',
       template: 'treemap',
@@ -93,4 +96,4 @@ export default ({command}) => ({
     port: 3000,
     strictPort: true,
   }
-});
+}));
