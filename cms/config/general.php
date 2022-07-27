45e671fd928f7b871c24e24aa70ecc27272f4b3b
Andrew Welch <andrew@nystudio107.com>
2022-07-27 14:36:23 -0400

    refactor: Use fluent model config

--- cms/config/general.php
+++ cms/config/general.php
@@ -8,45 +8,44 @@
  * @see craft\config\GeneralConfig
  */
 
+use craft\config\GeneralConfig;
 use craft\helpers\App;
 
-return [
+return GeneralConfig::create()
     // Craft config settings from .env variables
-    'aliases' => [
+    ->aliases([
         '@assetsUrl' => App::env('ASSETS_URL'),
         '@cloudfrontUrl' => App::env('CLOUDFRONT_URL'),
         '@transcodedEpisodes' => 'https://d21kofsrtbjnsd.cloudfront.net/',
         '@ptGoogleAnalyticsProxy' => 'https://dbt8fhq6c1i4k.cloudfront.net/',
         '@web' => App::env('SITE_URL'),
         '@webroot' => App::env('WEB_ROOT_PATH'),
-    ],
-    'allowUpdates' => (bool)App::env('ALLOW_UPDATES'),
-    'allowAdminChanges' => (bool)App::env('ALLOW_ADMIN_CHANGES'),
-    'backupOnUpdate' => (bool)App::env('BACKUP_ON_UPDATE'),
-    'devMode' => (bool)App::env('DEV_MODE'),
-    'enableTemplateCaching' => (bool)App::env('ENABLE_TEMPLATE_CACHING'),
-    'resourceBasePath' => App::env('WEB_ROOT_PATH') . '/cpresources',
-    'runQueueAutomatically' => (bool)App::env('RUN_QUEUE_AUTOMATICALLY'),
-    'securityKey' => App::env('SECURITY_KEY'),
+    ])
+    ->allowUpdates(App::env('ALLOW_UPDATES'))
+    ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES'))
+    ->backupOnUpdate(App::env('BACKUP_ON_UPDATE'))
+    ->devMode(App::env('DEV_MODE'))
+    ->enableTemplateCaching(App::env('ENABLE_TEMPLATE_CACHING'))
+    ->resourceBasePath(App::env('WEB_ROOT_PATH') . '/cpresources')
+    ->runQueueAutomatically(App::env('RUN_QUEUE_AUTOMATICALLY'))
+    ->securityKey(App::env('SECURITY_KEY'))
     // Craft config settings from constants
-    'cacheDuration' => false,
-    'defaultSearchTermOptions' => [
+    ->cacheDuration(false)
+    ->defaultSearchTermOptions([
         'subLeft' => true,
         'subRight' => true,
-    ],
-    'defaultTokenDuration' => 'P2W',
-    'enableCsrfProtection' => true,
-    'errorTemplatePrefix' => 'errors/',
-    'extraFileKinds' => [
-        // merge .csv into list of text file kinds
+    ])
+    ->defaultTokenDuration('P2W')
+    ->enableCsrfProtection(true)
+    ->errorTemplatePrefix('errors/')
+    ->extraFileKinds([
         'text' => [
             'extensions' => ['csv'],
         ],
-    ],
-    'generateTransformsBeforePageLoad' => true,
-    'maxCachedCloudImageSize' => 3000,
-    'maxUploadFileSize' => '100M',
-    'omitScriptNameInUrls' => true,
-    'useEmailAsUsername' => true,
-    'usePathInfo' => true,
-];
+    ])
+    ->generateTransformsBeforePageLoad(true)
+    ->maxCachedCloudImageSize(3000)
+    ->maxUploadFileSize(100 * 1024 * 1024)
+    ->omitScriptNameInUrls(true)
+    ->useEmailAsUsername(true)
+    ->usePathInfo(true);
