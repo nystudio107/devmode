a488f8bcacb54802d2bf8db9ecd751fa0b28fc5e
Andrew Welch <andrew@nystudio107.com>
2025-05-05 19:41:34 -0400

    refactor: Updated deps & pass in an empty array for `css` to `critical`

--- buildchain/vite.config.ts
+++ buildchain/vite.config.ts
@@ -92,7 +92,9 @@ export default defineConfig(({command}) => ({
         {uri: 'errors/503', template: 'errors/503'},
         {uri: 'errors/404', template: 'errors/404'},
       ],
-      criticalConfig: {}
+      criticalConfig: {
+        css: [],
+      }
     }),
   ],
   optimizeDeps: {
