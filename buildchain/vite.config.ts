5a98b8f6c4d8971b06df512889b4e7556b78937b
Andrew Welch <andrew@nystudio107.com>
2025-05-05 14:48:07 -0400

    refactor: Re-enable Critical CSS

--- buildchain/vite.config.ts
+++ buildchain/vite.config.ts
@@ -77,7 +77,6 @@ export default defineConfig(({command}) => ({
       typescript: true,
       vueTsc: true,
     }),
-    /* Temporarily disable critical css to test buddy.works
     critical({
       criticalUrl: 'https://devmode.fm/',
       criticalBase: '../cms/web/dist/criticalcss/',
@@ -94,7 +93,6 @@ export default defineConfig(({command}) => ({
       ],
       criticalConfig: {}
     }),
-    */
   ],
   optimizeDeps: {
     include: ['axios'],
